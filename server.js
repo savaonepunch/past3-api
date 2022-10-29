e2caae8ecd9d0557f7027d933ab5a555ae075c49
O9server <katramadoss@gmail.com>
2022-10-29 21:12:07 +0300

    changed port to env

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
     origin: '*',
