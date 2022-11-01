df78243d4e3dbccb67048038a7cc9d36bad740a5
O9server <katramadoss@gmail.com>
2022-11-01 21:49:42 +0200

    webhook error handle

--- routes/pastes.js
+++ routes/pastes.js
@@ -1,9 +1,9 @@
-const { json } = require('body-parser');
-const express = require('express');
+const { json } = require("body-parser");
+const express = require("express");
 const router = express.Router();
-const { Webhook, MessageBuilder } = require('discord-webhook-node');
+const { Webhook, MessageBuilder } = require("discord-webhook-node");
 
-const Paste = require('../models/Paste');
+const Paste = require("../models/Paste");
 
 const hook = new Webhook(process.env.DISCORD_WEBHOOK);
 
@@ -18,45 +18,47 @@ const hook = new Webhook(process.env.DISCORD_WEBHOOK);
 // });
 
 // Get a paste
-router.get('/:id', async (req, res) => {
-    try {
-        const paste = await Paste.find({
-            "_id": req.params.id
-        });
-        if (!paste.length) {
-            return res.status(404).json({ message: "Could not find paste" });
-        } else {
-            res.json(paste[0]);
-        }
-    } catch (err) {
-        res.status(500).json({ message: err.message });
+router.get("/:id", async (req, res) => {
+  try {
+    const paste = await Paste.find({
+      _id: req.params.id,
+    });
+    if (!paste.length) {
+      return res.status(404).json({ message: "Could not find paste" });
+    } else {
+      res.json(paste[0]);
     }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 // Create a paste
-router.post('/', async (req, res) => {
-    const paste = new Paste({
-        title: req.body.title,
-        author: req.body.author,
-        paste: req.body.paste,
-        syntax: req.body.syntax,
-    })
+router.post("/", async (req, res) => {
+  const paste = new Paste({
+    title: req.body.title,
+    author: req.body.author,
+    paste: req.body.paste,
+    syntax: req.body.syntax,
+  });
 
-    try {
-        const newPaste = await paste.save();
-        res.status(201).json({ newPaste });
-        const embed = new MessageBuilder()
-            .setTitle(newPaste.title)
-            .setAuthor(newPaste.author)
-            .setURL(`https://past3.netlify.app/pastes/${ newPaste._id }`)
-            .setColor('#fff')
-            .setDescription(newPaste.paste)
-            .setTimestamp(newPaste.addedDate);
-        await hook.send(embed);
-
-    } catch (error) {
-        res.status(400).json({ error });
-    }
-})
+  try {
+    const newPaste = await paste.save();
+    res.status(201).json({ newPaste });
+    const embed = new MessageBuilder()
+      .setTitle(newPaste.title)
+      .setAuthor(newPaste.author)
+      .setURL(`https://past3.netlify.app/pastes/${newPaste._id}`)
+      .setColor("#fff")
+      .setDescription(newPaste.paste)
+      .setTimestamp(newPaste.addedDate);
+    await hook
+      .send(embed)
+      .then(() => console.log("Sent webhook successfully!"))
+      .catch((err) => console.log(err.message));
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
 
 module.exports = router;
